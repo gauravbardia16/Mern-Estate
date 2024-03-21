ca46ed8628a9c346c79b30179d13e643a341bcce
gauravbardia16 <gauravbardia111@gmail.com>
2024-03-21 19:02:57 +0530

    Sign Out

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -10,6 +10,7 @@ import {
   deleteUserFailure,
   deleteUserStart,
   deleteUserSuccess,
+  signOutUserStart,
 } from '../redux/user/userSlice';
 import { useDispatch } from 'react-redux';
 
@@ -103,6 +104,22 @@ const dispatch = useDispatch();
           dispatch(deleteUserFailure(error.message));
         }
       };
+
+      const handleSignOut = async () => {
+        try {
+          dispatch(signOutUserStart());
+          const res = await fetch('/api/auth/signout');
+          const data = await res.json();
+          if (data.success === false) {
+            dispatch(deleteUserFailure(data.message));
+            return;
+          }
+          dispatch(deleteUserSuccess(data));
+        } catch (error) {
+          dispatch(deleteUserFailure(data.message));
+        }
+      };
+
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl font-semibold text-center my-7'>Profile</h1>
@@ -146,7 +163,7 @@ const dispatch = useDispatch();
           Delete Account
         </span>
         
-        <span className='text-red-700 cursor-pointer'>
+        <span onClick={handleSignOut} className='text-red-700 cursor-pointer'>
           Sign Out
         </span>
       </div>
