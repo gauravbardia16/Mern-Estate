b4cd0e46b965dc0187b639e9af693b03dc9c6073
gauravbardia16 <gauravbardia111@gmail.com>
2024-03-22 01:39:55 +0530

    Completed Creation of listing Page

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -13,7 +13,7 @@ import {
   signOutUserStart,
 } from '../redux/user/userSlice';
 import { useDispatch } from 'react-redux';
-
+import {Link} from 'react-router-dom';
 export default function Profile() {
   const fileRef = useRef(null)
   const{currentUser, loading, error} = useSelector((state) => state.user)
@@ -156,7 +156,12 @@ const dispatch = useDispatch();
         <button disabled={loading} className='bg-slate-700 text-white rounded-large p-3 uppercase hover:opacity-95 disabled:opacity-80'>
         {loading ? 'Loading...' : 'Update'}
         </button>
-
+        <Link
+          className='bg-green-700 text-white p-3 rounded-lg uppercase text-center hover:opacity-95'
+          to={'/create-listing'}
+        >
+          Create Listing
+        </Link>
       </form>
       <div className='flex justify-between mt-5'>
         <span onClick={handleDeleteUser} className='text-red-700 cursor-pointer'>
